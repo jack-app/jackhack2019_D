cbd35500c0d087e11ea28f3baf1c9f12d4546e4a
NakaiKota <active3min.tk@gmail.com>
2019-05-04 16:08:55 +0900

    button スクリプトを追加

---
+++ jackhack2019_D/Assets/kumaScripts/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    //変数宣言
+    public GameObject[] ObjectToActivate, ObjectToInactivate;
+
+    // 有効化
+    public void Activate (){
+        foreach(GameObject act in ObjectToActivate){
+            act.SetActive(true);
+        }
+    }
+
+    // 無効化
+    public void Inactivate (){
+        foreach(GameObject inact in ObjectToInactivate){
+            inact.SetActive(false);
+        }
+    }
+}
